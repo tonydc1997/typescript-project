ba9c8e2460bdf8a1b54cca75cf091b26bd634ab6
Anthony Curtis <tonydc1997@gmail.com>
2018-09-20 12:27:18 -0700

    Explore number type syntax correction

--- typescript.ts
+++ typescript.ts
@@ -6,5 +6,10 @@ let answer = sumOfTwoNumbers(9, 9);
 
 console.log(answer);
 
+// TYPES IN TYPESCRIPT
+
 // Boolean
 let enteringFlowState: boolean = true;
+
+// Number 
+let someonesAge: number = 'hello'
