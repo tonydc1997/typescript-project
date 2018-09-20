b1a9cc62bd84f19cc5eb964f0abaa4d66a41c277
Anthony Curtis <tonydc1997@gmail.com>
2018-09-20 12:25:00 -0700

    Learn about the different types in typescript

--- typescript.js
+++ typescript.js
@@ -1,5 +1,7 @@
 var sumOfTwoNumbers = function (a, b) {
     return a + b;
 };
-var answer = sumOfTwoNumbers(4, 9);
+var answer = sumOfTwoNumbers(9, 9);
 console.log(answer);
+// Boolean
+var enteringFlowState = true;
--- typescript.ts
+++ typescript.ts
@@ -2,6 +2,9 @@ const sumOfTwoNumbers = (a: number, b: number) => {
   return a + b; 
 }
 
-let answer = sumOfTwoNumbers(4, 9);
+let answer = sumOfTwoNumbers(9, 9);
 
 console.log(answer);
+
+// Boolean
+let enteringFlowState: boolean = true;
